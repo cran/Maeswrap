0e90ca7762c58d2f3690ec43700c16102c37f05e
Remko Duursma <remkoduursma@gmail.com>
2011-09-21 00:00:00 +0000

    version 1.4.8

---
+++ R/readmet.R
@@ -0,0 +1,19 @@
+`readmet` <-
+function(filename="met.dat", nlines=-1){
+
+	metlines <- readLines(filename)
+	namesloc <- grep("columns",tolower(metlines))
+	namesloc <- setdiff(namesloc,grep("nocolumns",metlines))
+	
+	namesline <- metlines[namesloc]
+	datastart <- grep("DATA STARTS", metlines)
+	sp <- strsplit(namesline, "=")[[1]][2]
+	NAMES <- delempty(trim(strsplit(sp, "\t")[[1]]))
+	NAMES <- gsub("'", "", NAMES)
+	NAMES <- do.call("c", strsplit(NAMES, " ", fixed=TRUE))
+	metdata <- read.table(filename, header=FALSE, skip=datastart, nrows=nlines)
+	names(metdata) <- NAMES
+
+return(metdata)
+}
+
